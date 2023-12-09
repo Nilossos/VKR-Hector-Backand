80ff473ba3398df4ce5c85cc627fd332e26382dc
IW0N <world9655@gmail.com>
2023-12-09 16:27:17 +0300

    создан сервис для расчёта расстояний между целевыми точками

---
+++ Backand/Services/DistanceService.cs
@@ -0,0 +1,51 @@
+﻿using Backand.Services.WebDriverServiceSpace;
+using OpenQA.Selenium;
+using static Newtonsoft.Json.JsonConvert;
+
+namespace Backand.Services
+{
+    internal class DistanceService
+    {
+        IWebElement input;
+        IWebElement output;
+        IWebElement reset_btn;
+        public DistanceService(WebDriver driver)
+        {
+            input = driver.FindElement(By.Id("input"));
+            output = driver.FindElement(By.Id("output"));
+            reset_btn = driver.FindElement(By.Id("reset"));
+        }
+        public DistanceService(IServiceProvider provider) : this(provider.GetRequiredService<WebDriverService>().Value)
+        {
+
+        }
+        
+        public async Task<double> GetDistance(params double[][] routes)
+        {
+            input.SendKeys(SerializeObject(routes));
+            output.Click();
+            double distance = await Task.Run(() =>
+            {
+                while (output.GetAttribute("read") == "0") { }
+                string dist_str = output.GetAttribute("value");
+                double distance = DeserializeObject<double>(dist_str);
+                if (distance > -1)
+                    return distance;
+                else
+                {
+                    string error=output.GetAttribute("error");
+                    throw new Exception(error);
+                }
+            });
+            reset_btn.Click();
+            return distance;
+        }
+    }
+    public static class DistanceServiceExtension
+    {
+        public static void AddDistanceService(this IServiceCollection services)
+        {
+            services.AddSingleton(x => new DistanceService(x));
+        }
+    }
+}
---
+++ Backand/Services/WebDriverService/WebDriverProps.cs
@@ -0,0 +1,28 @@
+﻿namespace Backand.Services.WebDriverServiceSpace
+{
+    public readonly struct WebDriverProps
+    {
+        public string DriverPath { get; }
+        public WebDriverType DriverType { get; }
+        public string HtmlPath { get; }
+        public string YamapsScriptLink { get; }
+        public WebDriverProps(IConfiguration config)
+        {
+            IConfiguration custom = config.GetSection("Custom");
+            DriverPath = custom["Engine:driverPath"]??"";
+            HtmlPath = custom["distanceHtml"]??"";
+            DriverType = WDTypesTable.Table[custom["Engine:type"]];
+            YamapsScriptLink = GetScriptLink(config);
+        }
+        private string GetScriptLink(IConfiguration config)
+        {
+            IConfiguration yamaps = config.GetSection("yamaps");
+            string protocol = yamaps["protocol"];
+            string domain = yamaps["domain"];
+            string version = yamaps["version"];
+            string apikey = yamaps["apikey"];
+            string src = $"{protocol}://{domain}/{version}/?apikey={apikey}&lang=en_RU";
+            return src;
+        }
+    }
+}
