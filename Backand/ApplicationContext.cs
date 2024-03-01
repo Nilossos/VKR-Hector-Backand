37d60ff0d507b626b2668c0c7e4157e0c6ac0829
IW0N <world9655@gmail.com>
2024-03-01 20:43:40 +0300

    ddd

--- Backand/ApplicationContext.cs
+++ Backand/ApplicationContext.cs
@@ -78,9 +78,8 @@ namespace Backand
 
 		public virtual DbSet<UserType> UserType { get; set; }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-			=> optionsBuilder.UseNpgsql("Host=localhost;Database=gazprom_db;Username=postgres;Password=postgres");
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=> 
+			optionsBuilder.UseNpgsql(AppContextOptions.Options);
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
