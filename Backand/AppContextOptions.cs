4157e988e160c7eec2d3d2aadee51aa68f600502
as <as@uralmicro.ru>
2024-10-14 19:47:42 +0500

    Изменил gitignore

--- Backand/AppContextOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backand
-{
-    public static class AppContextOptions
-    {
-        public static string Options { get => "Host=localhost;Port=5432;Database=gazprom_db2;Username=postgres;Password=2051"; }
-        
-    }
-}
