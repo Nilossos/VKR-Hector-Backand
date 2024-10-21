4cb2f868d3a1d7970005d50b9c0c41faa0923071
as <as@uralmicro.ru>
2024-10-21 16:07:07 +0500

    gitignore update

--- Backand/AppContextOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backand
-{
-    public static class AppContextOptions
-    {
-        public static string Options { get => "Host=localhost;Port=5432;Database=hector_db;Username=postgres;Password=qwerty"; }
-        
-    }
-}
